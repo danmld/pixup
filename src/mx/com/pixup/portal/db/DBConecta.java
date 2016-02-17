7aa0a2e266af5cbe568779170a9992ad7d203473
AzureLion <rodrigo.vazquez.lion@gmail.com>
2016-02-17 14:30:53 -0600

    Update DBConecta.java

--- src/mx/com/pixup/portal/db/DBConecta.java
+++ src/mx/com/pixup/portal/db/DBConecta.java
@@ -6,7 +6,6 @@
 package mx.com.pixup.portal.db;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Enumeration;
@@ -29,11 +28,6 @@ public class DBConecta {
     private Properties dbProp = new Properties();
 
     /**
-     * Objeto de Conexion a la base de datos
-     */
-    protected Connection oConn = null;
-
-    /**
      * Statement para preparar las consultas
      */
     protected Statement stmt = null;
@@ -48,14 +42,14 @@ public class DBConecta {
      * Lee los parámetros de conexion del archivo JDBC.properties y los guarda en en dbProp
      */
     public DBConecta() {
-        ResourceBundle rsb = ResourceBundle.getBundle("JDBC");
+        ResourceBundle rsb = ResourceBundle.getBundle("setup");
 
         Enumeration enum1 = rsb.getKeys();
         Properties dbProp = new Properties();
         while (enum1.hasMoreElements()) {
             String sTmp = (String) enum1.nextElement();
             dbProp.setProperty(sTmp, rsb.getString(sTmp));
-            System.out.println("+" + sTmp );
+            //System.out.println("+" + sTmp );
         }
         this.dbProp = dbProp;
         System.out.println(dbProp);
@@ -76,10 +70,10 @@ public class DBConecta {
         DBConecta db = DBConecta.getInstance();
         
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName(db.dbProp.getProperty("MySql.driver"));
-        dataSource.setUsername(db.dbProp.getProperty("MySql.username"));
-        dataSource.setPassword(db.dbProp.getProperty("MySql.password"));
-        dataSource.setUrl(db.dbProp.getProperty("MySql.url"));
+        dataSource.setDriverClassName(db.dbProp.getProperty("dataSource.className"));
+        dataSource.setUsername(db.dbProp.getProperty("dataSource.username"));
+        dataSource.setPassword(db.dbProp.getProperty("dataSource.password"));
+        dataSource.setUrl(db.dbProp.getProperty("dataSource.url"));
         
         return dataSource;
     }
@@ -97,52 +91,5 @@ public class DBConecta {
         return conn;
     }
 
-    /**
-     * Se conecta a la base de datos mysql con los parametros del JDBC.properties
-     */
-    public void conecta() {
-        try {
-            //
-            Class.forName(this.dbProp.getProperty("MySql.driver"));
-            this.oConn = DriverManager.getConnection(
-                    this.dbProp.getProperty("MySql.url"),
-                    this.dbProp.getProperty("MySql.username"),
-                    this.dbProp.getProperty("MySql.password"));
-            this.stmt = oConn.createStatement();
-        } catch (ClassNotFoundException | SQLException ex) {
-            Logger.getLogger(DBConecta.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-    }
-    /**
-     * Se conecta a la base de datos access con los parametros del JDBC.properties
-     */
-    public void conectaAccess() {
-        try {
-            //
-            Class.forName(this.dbProp.getProperty("access.driver"));
-            this.oConn = DriverManager.getConnection(
-                    this.dbProp.getProperty("access.url"),
-                    this.dbProp.getProperty("access.username"),
-                    this.dbProp.getProperty("access.password"));
-            this.stmt = oConn.createStatement();
-        } catch (ClassNotFoundException | SQLException ex) {
-            Logger.getLogger(DBConecta.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-    }
-    /**
-     * Cierra la conexión para evitar dejar conexiones abiertas
-     */
-    public void desconecta(){
-        try {
-            if( !this.oConn.isClosed() ){
-                oConn.close();
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(DBConecta.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
     
 }
